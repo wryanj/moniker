0d45ba44a27f44f42128f543a4cc7b67643640f0
wryanj <ryanjohnson9685@gmail.com>
2021-06-30 17:49:49 -0400

    nametype filters updated to be toggle changes and support future filter changes of items in name array

--- client/src/components/NameTypeFilter/index.js
+++ client/src/components/NameTypeFilter/index.js
@@ -2,32 +2,91 @@
 /*                             Import Dependencies                            */
 /* -------------------------------------------------------------------------- */
 
-import React from "react";
+    import React, { useEffect, useState } from "react"; 
 
 /* -------------------------------------------------------------------------- */
 /*                              Define Component                              */
 /* -------------------------------------------------------------------------- */
-
     function NameTypeFilter() {
+
+        /* ---------------------------------- State --------------------------------- */
+
+        const [isAllShowing, setShowAll] = useState(true)
+        const [isFirstShowing, setisFirstShowing] = useState(false)
+        const [isMiddleShowing, setisMiddleShowing] = useState(false)
+        const [isFullShowing, setisFullShowing] = useState(false)
+
+        /* ---------------------------------- Logic --------------------------------- */
+        // Show Ticket Details
+        function handleShowAll () {
+            // Set this showing state to true and others false
+            setShowAll(true);
+            setisFirstShowing(false);
+            setisMiddleShowing(false);
+            setisFullShowing(false);
+        } 
+        // Show Ticket Details
+         function handleShowFirst () {
+            // Set this showing state to true and others false
+            setShowAll(false);
+            setisFirstShowing(true);
+            setisMiddleShowing(false);
+            setisFullShowing(false);
+        }
+
+        // Show Client Details
+        function handleShowMiddle () {
+            // Set this state true and others false
+            setShowAll(false);
+            setisFirstShowing(false);
+            setisMiddleShowing(true);
+            setisFullShowing(false);
+        }
+
+        // Show Client Details
+        function handleShowFull () {
+            // Set this state true and others false
+            setShowAll(false);
+            setisFirstShowing(false);
+            setisMiddleShowing(false);
+            setisFullShowing(true);
+        }
+
+        /* ---------------------------- Render Component ---------------------------- */
         return (
-            <div className="text-center">
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="firstnameFilter" value="first"/>
-                    <label className="form-check-label" htmlFor="firstnameFilter">All</label>
-                </div>
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="fullnameFilter" value="full"/>
-                    <label className="form-check-label" htmlFor="fullnameFilter">Full</label>
-                </div>
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="firstnameFilter" value="first"/>
-                    <label className="form-check-label" htmlFor="firstnameFilter">First</label>
-                </div>
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="middlenameFilter" value="middle"/>
-                    <label className="form-check-label" htmlFor="middlenameFilter">Middle</label>
-                </div>
+        
+            <div className="row mb-3 justify-content-center">
+                            <div className="col text-center">
+                               <button 
+                                    className={isAllShowing ? "btn alert-info btn-sm text-center shadow-sm" : "btn btn-light btn-sm text-center shadow-sm"} 
+                                    onClick={handleShowAll}>
+                                        All
+                                </button>
+                           </div>
+                           <div className="col text-center">
+                               <button 
+                                    className={isFirstShowing ? "btn alert-info btn-sm text-center shadow-sm" : "btn btn-light btn-sm text-center shadow-sm"} 
+                                    onClick={handleShowFirst}>
+                                        First
+                                </button>
+                           </div>
+                           <div className="col text-center">
+                               <button 
+                                    className={isMiddleShowing ? "btn alert-info btn-sm text-center shadow-sm" : "btn btn-light btn-sm text-center shadow-sm"} 
+                                    onClick={handleShowMiddle}>
+                                        Middle
+                                </button>
+                           </div>
+                           <div className="col text-center">
+                               <button 
+                                    className={isFullShowing ? "btn alert-info btn-sm text-center shadow-sm" : "btn btn-light btn-sm text-center shadow-sm"} 
+                                    onClick={handleShowFull}>
+                                        Full
+                                </button>
+                            </div>
             </div>
+            
+            
         );
     }
 
