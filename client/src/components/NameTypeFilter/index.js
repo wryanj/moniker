01df55854f77a7f5ffd531ce306ab30728173c09
wryanj <ryanjohnson9685@gmail.com>
2021-06-30 16:23:01 -0400

    added nametype filter for all and default

--- client/src/components/NameTypeFilter/index.js
+++ client/src/components/NameTypeFilter/index.js
@@ -13,15 +13,19 @@ import React from "react";
             <div className="text-center">
                 <div className="form-check form-check-inline">
                     <input className="form-check-input" type="radio" name="inlineRadioOptions" id="firstnameFilter" value="first"/>
+                    <label className="form-check-label" htmlFor="firstnameFilter">All</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="fullnameFilter" value="full"/>
+                    <label className="form-check-label" htmlFor="fullnameFilter">Full</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="firstnameFilter" value="first"/>
                     <label className="form-check-label" htmlFor="firstnameFilter">First</label>
                 </div>
                 <div className="form-check form-check-inline">
                     <input className="form-check-input" type="radio" name="inlineRadioOptions" id="middlenameFilter" value="middle"/>
                     <label className="form-check-label" htmlFor="middlenameFilter">Middle</label>
-                </div>
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="fullnameFilter" value="full"/>
-                    <label className="form-check-label" htmlFor="fullnameFilter">Full</label>
                 </div>
             </div>
         );
